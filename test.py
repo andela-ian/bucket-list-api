83bf2ac94a345385486a107b30240fffee7c7577
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-10 08:39:25 +0100

    (fix) indent testrunner; use string in dictionary keys; use pep-8 formatting

--- test.py
+++ test.py
@@ -3,30 +3,37 @@ import unittest
 
 
 class BucketListTestCase(unittest.TestCase):
-    
+
     def setUp(self):
         self.client = app.test_client(self)
 
     # Test endpoints
-    
+
     # Ensure that login loads correctly
     def test_login_loads(self):
         response = self.client.get('/login')
         self.assertEqual(response.status_code, 200)
-    
-    # Ensure that correct login behaves correctly 
+
+    # Ensure that correct login behaves correctly
     def test_correct_login(self):
         response = self.client.get('/login')
         self.assertTrue(response.status_code == 200)
-        response = self.client.post('/login', {username:'john', password:'oldman'}, follow_redirects=True)
+        response = self.client.post(
+            '/login',
+            {'username': 'john', 'password': 'oldman'},
+            follow_redirects=True
+            )
         self.assertTrue(response.status_code == 200)
-    
+
     # Ensure that logout behave correctly
     def test_logout(self):
-        self.client.post('/login', {username:'john', password:'oldman'})
+        self.client.post(
+            '/login',
+            {'username': 'john', 'password': 'oldman'}
+            )
         response = self.client.get('/logout', follow_redirects=True)
         self.assertTrue(response.status_code == 200)
 
 
- if __name__ == '__main__':
-     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
