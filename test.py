bb669f307a3ad1608ad538a1dee056adead27791
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-11 17:47:06 +0100

    (fix) rename response to rv to fix variable name conflicts; replace assertTrue  with assertEqual; change to using keyword args as second argument in post method of test client; setup test client with a call to create app which returns the bucketlist app instance

--- test.py
+++ test.py
@@ -1,38 +1,31 @@
-from app import app
+from app import create_app
 import unittest
 
 
 class BucketListTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client(self)
+        bucket_list_app = create_app()
+        self.client = bucket_list_app.test_client(self)
 
     # Test endpoints
 
-    # Ensure that login loads correctly
-    def test_login_loads(self):
-        response = self.client.get('/login')
-        self.assertEqual(response.status_code, 200)
-
     # Ensure that correct login behaves correctly
     def test_correct_login(self):
-        response = self.client.get('/login')
-        self.assertTrue(response.status_code == 200)
-        response = self.client.post(
+        rv = self.client.post(
             '/login',
-            {'username': 'john', 'password': 'oldman'},
-            follow_redirects=True
+            data={'username': 'john', 'password': 'oldman'}
             )
-        self.assertTrue(response.status_code == 200)
+        self.assertEqual(rv.status_code, 200)
 
     # Ensure that logout behave correctly
     def test_logout(self):
         self.client.post(
             '/login',
-            {'username': 'john', 'password': 'oldman'}
+            data={'username': 'john', 'password': 'oldman'}
             )
-        response = self.client.get('/logout', follow_redirects=True)
-        self.assertTrue(response.status_code == 200)
+        rv = self.client.get('/logout')
+        self.assertEqual(rv.status_code, 200)
 
 
 if __name__ == '__main__':
