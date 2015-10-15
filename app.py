c14f3b75a89ae3d77e18ed7bc2434bdbd71a2f70
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-15 12:11:50 +0100

    (feature) wrap app instance maker inside a function for ease of handling and to workaround issue with cyclic imports

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response, request, session
-from models import db
+from flask_restful import Resource, Api
+from models import db, BucketList, User
 import auth
 
 
@@ -7,6 +8,7 @@ def create_app(config_module=None):
     app = Flask(__name__)
     app.config.from_object(config_module or 'config')
     db.init_app(app)
+    api = Api(app)
 
     @app.route('/login', methods=['POST'])
     def login():
@@ -24,12 +26,25 @@ def create_app(config_module=None):
         session.pop('user', None)
         return make_response(jsonify({'message': 'Logged out successfully'}))
 
-    @app.route('/home', methods=['GET'])
-    @auth.requires_auth
-    def index():
-        return make_response(jsonify({}), 200)
+    class BucketListMaster(Resource):
+        def get(self):
+            user_id = auth.get_current_user_id()
+            return {'data': db.session
+                              .query(BucketList)
+                              .filter(user_id == user_id)
+                              .all()}
+
+        def post(self):
+            user_id = auth.get_current_user_id()
+            todo = request.form.get('todo')
+            db.session.add(BucketList(user_id, todo))
+            db.session.commit()
+            return {'data': 'hello'}, 201
+
+    api.add_resource(BucketListMaster, '/bucketlists')
 
     return app
+
 
 if __name__ == '__main__':
     bucket_list_app = create_app()
