1ce532d998b92788e8d57c3bb382d4fc2d2f2ca9
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-15 12:08:13 +0100

    (feature) test that a user can retrieve his/her stored bucketlist items

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import unittest
 class BucketListTestCase(unittest.TestCase):
 
     def setUp(self):
-        bucket_list_app = create_app()
-        self.client = bucket_list_app.test_client(self)
+        bucketlist_app = create_app()
+        self.client = bucketlist_app.test_client(self)
 
     # Test endpoints
 
@@ -27,6 +27,27 @@ class BucketListTestCase(unittest.TestCase):
         rv = self.client.get('/logout')
         self.assertEqual(rv.status_code, 200)
 
+    # Ensure that user can retrieve his/her bucketlist items
+    def test_user_can_retrieve_bucketlist_items(self):
+        self.client.post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        rv = self.client.get('/bucketlist')
+        self.assertIn("No bucketlist items were found", rv.data)
+        self.client.get('/logout')
+
+    # Ensure user can create new bucketlist item
+    def test_user_can_create_new_bucketlist_item(self):
+        self.client.post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        rv = self.client.post(
+            '/bucketlist',
+            data={'todo': 'Witness a miracle'})
+        self.assertEqual(rv.status_code, 201)
+        self.client.get('/logout')
 
 if __name__ == '__main__':
     unittest.main()
