943c90ab3fa8252267d2bf64747d7d3fdfe99961
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-23 09:08:51 +0100

    (feature) user can access these endpoints: POST /login, GET /logout, GET /bucketlists, POST /bucketlists, GET /bucketlists/<id>, PUT /bucketlists/<id>, DELETE /bucketlists/<id>

--- app.py
+++ app.py
@@ -1,50 +1,92 @@
-from flask import Flask, jsonify, make_response, request, session
-from flask_restful import Resource, Api
-from models import db, BucketList, User
+from flask.ext.api import FlaskAPI
+from flask.ext.api.exceptions import APIException, \
+    AuthenticationFailed, NotFound, PermissionDenied
+from flask import request
+from models import db, BucketList
+from tools import list_object_transform
 import auth
 
 
-def create_app(config_module=None):
-    app = Flask(__name__)
-    app.config.from_object(config_module or 'config')
-    db.init_app(app)
-    api = Api(app)
+class CredentialsRequired(APIException):
+    """Raises a 202 accepted when the user first accesses GET /login"""
+    status_code = 202
+    detail = "Make a POST to '/login' with your credentials to begin a session"
 
-    @app.route('/login', methods=['POST'])
+
+def create_app(config_module="config.DevelopmentConfig"):
+    app = FlaskAPI(__name__)
+    app.config.from_object(config_module)
+    db.init_app(app)
+
+    @app.route("/login", methods=["GET", "POST"])
     def login():
-        username = request.form.get('username')
-        password = request.form.get('password')
+        if request.method == "GET":
+            raise CredentialsRequired()
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         if auth.check_auth(username, password):
-            session['user'] = [username, password]
-            return make_response(
-                jsonify({'message': 'Logged in successfully'}), 200)
+            return {
+                "message": [
+                    auth.MESSAGES["login"],
+                    {
+                        "available endpoints":
+                        app.config.get('AVAILABLE_ENDPOINTS')
+                    }
+                ],
+                "token": auth.tokenize(username, password)
+            }
+        else:
+            raise AuthenticationFailed()
 
-    @app.route('/logout', methods=['GET'])
+    @app.route("/logout", methods=["GET"])
     @auth.requires_auth
     def logout():
-        session.pop('user', None)
-        return make_response(jsonify({'message': 'Logged out successfully'}))
+        if auth.logout():
+            return {"message": auth.MESSAGES["logout"]}
+        raise NotFound()
 
-    class BucketListMaster(Resource):
-        def get(self):
-            user_id = auth.get_current_user_id()
-            return {'data': db.session
-                              .query(BucketList)
-                              .filter(user_id == user_id)
-                              .all()}
+    @app.route("/bucketlists", methods=["POST", "GET"])
+    @auth.requires_auth
+    def bucketlist():
+        user_id = auth.get_current_user_id()
+        if request.method == "GET":
+            query = db.session.query(BucketList)\
+                .filter(user_id == user_id).all()
+            if query:
+                result_data = list_object_transform(query)
+                return {'message': result_data}
+            raise NotFound()
 
-        def post(self):
-            user_id = auth.get_current_user_id()
-            todo = request.form.get('todo')
-            db.session.add(BucketList(user_id, todo))
+        else:
+            name = request.form.get("name")
+            a_bucketlist = BucketList(created_by=user_id, name=name)
+            db.session.add(a_bucketlist)
             db.session.commit()
-            return {'data': 'hello'}, 201
+            return {
+                "message": "Bucketlist was created successfully",
+                "bucketlist": a_bucketlist.to_json()}, 201
 
-    api.add_resource(BucketListMaster, '/bucketlists')
+    @app.route("/bucketlists/<id>", methods=["GET", "DELETE", "PUT"])
+    @auth.requires_auth
+    def actionable_bucketlist(id):
+        bucketlist = BucketList.query.get(int(id))
+        if bucketlist.created_by != auth.get_current_user_id():
+            raise PermissionDenied()
+
+        if request.method == "DELETE":
+            db.session.delete(bucketlist)
+            db.session.commit()
+            return {"message": "Bucketlist was deleted successfully"}, 410
+
+        elif request.method == "PUT":
+            name = request.form.get("name")
+            bucketlist.name = name
+            bucketlist.id = id
+            db.session.commit()
+        return bucketlist.to_json(), 200
 
     return app
-
 
 if __name__ == '__main__':
     bucket_list_app = create_app()
