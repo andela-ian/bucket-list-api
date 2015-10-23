44d0c3cee31e27a315c08de64d0a5433b81f41e4
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-23 09:07:12 +0100

    (chore) test for endpoints: POST /login, GET /logout, GET /bucketlists, POST /bucketlists, GET /bucketlists/<id>, PUT /bucketlists/<id>, DELETE /bucketlists/<id>

--- test.py
+++ test.py
@@ -1,53 +1,161 @@
+from models import db, User
 from app import create_app
+from auth import MESSAGES
 import unittest
+import os
+import json
 
 
 class BucketListTestCase(unittest.TestCase):
 
     def setUp(self):
-        bucketlist_app = create_app()
-        self.client = bucketlist_app.test_client(self)
+        self.user_data = {'username': 'john', 'password': 'oldman'}
+        bucketlist_app = create_app('config.TestingConfig')
+        self.app = bucketlist_app
+        self.client = bucketlist_app.test_client
+        with bucketlist_app.app_context():
+            db.create_all()
+            user = User(**self.user_data)
+            db.session.add(user)
+            db.session.commit()
 
     # Test endpoints
 
     # Ensure that correct login behaves correctly
     def test_correct_login(self):
-        rv = self.client.post(
+        rv = self.client().post(
             '/login',
-            data={'username': 'john', 'password': 'oldman'}
+            data=self.user_data
             )
         self.assertEqual(rv.status_code, 200)
+        self.assertIn(MESSAGES['login'], rv.data)
 
     # Ensure that logout behave correctly
     def test_logout(self):
-        self.client.post(
+        resp = self.client().post(
             '/login',
-            data={'username': 'john', 'password': 'oldman'}
+            data=self.user_data
             )
-        rv = self.client.get('/logout')
-        self.assertEqual(rv.status_code, 200)
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().get(
+            '/logout',
+            headers=headers)
+        self.assertIn(MESSAGES["logout"], rv.data)
 
     # Ensure that user can retrieve his/her bucketlist items
     def test_user_can_retrieve_bucketlist_items(self):
-        self.client.post(
+        resp = self.client().post(
             '/login',
             data={'username': 'john', 'password': 'oldman'}
             )
-        rv = self.client.get('/bucketlist')
-        self.assertIn("No bucketlist items were found", rv.data)
-        self.client.get('/logout')
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().get('/bucketlists', headers=headers)
+        self.assertEqual(rv.status_code, 404)
+
+        # write a bucket list and fetch
+        self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
+        rv = self.client().get('/bucketlists', headers=headers)
+        self.assertIn('Witness a miracle', rv.data)
+
+        self.client().get('/logout')
+
+    # Ensure user can create new bucketlist
+    def test_user_can_create_new_bucketlist(self):
+        resp = self.client().post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
+        self.assertEqual(rv.status_code, 201)
+        self.client().get('/logout')
+
+    # Ensure user can get buckletlist by id
+    def test_user_can_get_bucketlist_by_id(self):
+        resp = self.client().post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
+        rv = self.client().get(
+            '/bucketlists/1',
+            headers=headers)
+        self.assertEqual(rv.status_code, 200)
+
+    # Ensure user can update bucketlist by id
+    def test_user_can_update_bucketlist_by_id(self):
+        resp = self.client().post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {
+            'Authorization': 'Bearer {0}'.format(jwt_token),
+        }
+        self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
+        rv = self.client().put(
+            '/bucketlists/1',
+            data={'name': 'Witness the change'},
+            headers=headers)
+        self.assertEqual(rv.status_code, 200)
+
+    # Ensure user can delete bucketlist by id
+    def test_user_can_delete_bucketlist_by_id(self):
+        resp = self.client().post(
+            '/login',
+            data={'username': 'john', 'password': 'oldman'}
+            )
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {
+            'Authorization': 'Bearer {0}'.format(jwt_token),
+        }
+        self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
+        rv = self.client().delete(
+            '/bucketlists/1',
+            headers=headers)
+        self.assertEqual(rv.status_code, 410)
 
     # Ensure user can create new bucketlist item
     def test_user_can_create_new_bucketlist_item(self):
-        self.client.post(
+        resp = self.client().post(
             '/login',
             data={'username': 'john', 'password': 'oldman'}
             )
-        rv = self.client.post(
-            '/bucketlist',
-            data={'todo': 'Witness a miracle'})
+        resp_json = json.loads(resp.data)
+        jwt_token = resp_json.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().post(
+            '/bucketlists',
+            data={'name': 'Witness a miracle'}, headers=headers)
         self.assertEqual(rv.status_code, 201)
-        self.client.get('/logout')
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
+            # os.close(self.app.config.get('DB_FD'))
+            os.unlink(self.app.config.get('DATABASE'))
 
 if __name__ == '__main__':
     unittest.main()
