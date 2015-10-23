23ebee79ecd82c8adbff78aa49b5ef5f8dda573e
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-23 09:04:27 +0100

    (feature) method to transform object list to json

--- tools.py
+++ tools.py
@@ -1,2 +1,6 @@
-def decypher(session_var):
-    return tuple(session_var)
+def list_object_transform(list_data):
+    result_data = []
+    for item in list_data:
+        result_set = item.to_json()
+        result_data.append(result_set)
+    return result_data
