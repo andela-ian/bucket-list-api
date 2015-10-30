fa4cf335f0e712dd26a29f4148778ae1bc186263
Oluwafemi Sule <oluwafemi.sule@andela.com>
2015-10-30 08:54:13 +0100

    (fix) check that instance has to_json method before method is called on it

--- bucketlistapp/transformers/transform_to_dict.py
+++ bucketlistapp/transformers/transform_to_dict.py
@@ -3,6 +3,8 @@ def list_object_transform(list_data):
     """
     result_data = []
     for item in list_data:
-        result_set = item.to_json()
+        # import pdb; pdb.set_trace()
+        if callable(getattr(item, 'to_json')):
+            result_set = item.to_json()
         result_data.append(result_set)
     return result_data
